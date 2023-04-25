bd2b2dba02d3fbdc12b31b38f754faff7a243e69
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-04-25 12:19:53 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-//your JS code here. If required.
+
+
+function promise1(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("promisse1");
+        }, 1000);
+       });
+}
+function promise2(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("promise2");
+            
+        }, 2000);
+       });
+}
+       function promise3(){
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve("promise3");
+            }, 3000);
+           });
+    
+       }
+       Promise.all([promise1(),promise2(),promise3()]).then((res)=>{
+        console.log(res);
+       });
