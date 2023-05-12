cc7fef3ceb1cfb86acbbabe79d174d5a725a145e
saajanrana <123057148+saajanrana@users.noreply.github.com>
2023-05-12 01:38:35 -0400

    created a submission

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function promise1(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
           document.getElementsById("one").innerHTML=  resolve("promisse1");
+			document.getElementsById("two").innerHTML=  resolve("1sec"); 
         }, 1000);
        });
 }
@@ -11,7 +12,7 @@ function promise2(){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
         document.getElementsById("one").innerHTML=resolve("promise2");
-            
+            document.getElementsById("two").innerHTML=  resolve("2sec"); 
         }, 2000);
        });
 }
@@ -19,6 +20,8 @@ function promise2(){
         return new Promise((resolve, reject) => {
             setTimeout(() => {
               document.getElementsById("one").innerHTML =  resolve("promise3");
+document.getElementsById("two").innerHTML=  resolve("3sec"); 
+
             }, 3000);
            });
     
